e596bbfe5cf3f6b84074579e528ad21166cff0a1
Дмитрий Молько <molko_dmitriy@mail.ru>
2021-09-09 13:07:45 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using MyClass;
+
+namespace OOP_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terminal a = new Terminal();
+            a.Start();
+        }
+
+
+    }
+}
